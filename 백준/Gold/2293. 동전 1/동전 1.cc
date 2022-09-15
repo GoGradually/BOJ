857009f58e61da17e6c5c853e91e802b971dc200
GoGradually <62929862+GoGradually@users.noreply.github.com>
2022-09-15 17:08:52 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,34 +1,23 @@
-#include <bits/stdc++.h>
-#define MOD 1000000007
-#define ll long long
-#define INF 1e9 + 10
-#define lINF 1e18 + 10
+#include <cstring>
+#include <iostream>
+
 using namespace std;
 
-void solve() {
+int dp[10001];
+int arr[100];
+
+int main() {
     int n, k;
     cin >> n >> k;
-    ll dp[10001];
-    memset(dp, 0, sizeof(dp));
-    dp[0] = 1;
-    vector<ll> arr(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 1; j <= k; j++) {
-            if (j < arr[i]) continue;
-            dp[j] += dp[j - arr[i]];
+    dp[0] = 1;
+    for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= k; i++) {
+            if (arr[j] <= i) dp[i] += dp[i - arr[j]];
         }
     }
     cout << dp[k] << '\n';
-}
-
-int main(int argc, const char* argv[]) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    t = 1;
-    while (t--) solve();
     return 0;
 }

ca575fa0f769a4573fbfd67973af278c6c348915
GoGradually <62929862+GoGradually@users.noreply.github.com>
2022-10-27 16:33:23 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 852 ms, Memory: 81512 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007LL
+#define ll long long
+#define INF 1e9 + 10
+#define lINF 1e18 + 10
+using namespace std;
+
+int dp[1000001][2];
+vector<int> g[1000001];
+int go(int index, int ok, int start) {
+    if (dp[index][ok] != -1) return dp[index][ok];
+    if (index != start && g[index].size() == 1) return dp[index][ok] = ok;
+    int ret = ok;
+    for (int i = 0; i < g[index].size(); i++) {
+        int next = g[index][i];
+        if (next == start) continue;
+        if (ok == 0)
+            ret += go(next, 1, index);
+        else
+            ret += min(go(next, ok, index), go(next, 1 - ok, index));
+    }
+    return dp[index][ok] = ret;
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+    memset(dp, -1, sizeof(dp));
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    int start = 1;
+    cout << min(go(start, 0, start), go(start, 1, start)) << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}

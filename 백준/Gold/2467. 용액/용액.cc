d577b7135b58fcff3db50f470a3acb4374662ce3
GoGradually <62929862+GoGradually@users.noreply.github.com>
2022-10-28 13:30:39 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -13,34 +13,19 @@ void solve() {
         cin >> arr[i];
     }
     pair<int, int> ans;
-    int val = INF;
-    if (arr[0] >= 0) {
-        cout << arr[0] << ' ' << arr[1] << '\n';
-        return;
-    }
-    if (arr[n - 1] <= 0) {
-        cout << arr[n - 2] << ' ' << arr[n - 1] << '\n';
-        return;
-    }
-    for (int i = 0; i < n; i++) {
-        auto g2 = lower_bound(arr.begin(), arr.end(), -arr[i]);
-        int index = g2 - arr.begin();
-        if (index != n && index >= 0 && index != i &&
-            abs(arr[i] + arr[index]) < val) {
-            val = abs(arr[i] + arr[index]);
-            ans = {arr[i], arr[index]};
+    long long val = lINF;
+    int left = 0, right = n - 1;
+    while (left < right) {
+        if (val > abs(arr[left] + arr[right])) {
+            val = abs(arr[left] + arr[right]);
+            ans = {arr[left], arr[right]};
         }
-        index--;
-        if (index != n && index >= 0 && index != i &&
-            abs(arr[i] + arr[index]) < val) {
-            val = abs(arr[i] + arr[index]);
-            ans = {arr[i], arr[index]};
-        }
-        index--;
-        if (index != n && index >= 0 && index != i &&
-            abs(arr[i] + arr[index]) < val) {
-            val = abs(arr[i] + arr[index]);
-            ans = {arr[i], arr[index]};
+        if (arr[left] + arr[right] > 0) {
+            right--;
+        } else if (arr[left] + arr[right] < 0) {
+            left++;
+        } else {
+            break;
         }
     }
     cout << ans.first << ' ' << ans.second << '\n';

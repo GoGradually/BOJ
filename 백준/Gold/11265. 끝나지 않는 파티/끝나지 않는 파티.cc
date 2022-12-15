bc90a9680d435e00abdce1f1747023d320fa4749
GoGradually <62929862+GoGradually@users.noreply.github.com>
2022-12-15 09:17:36 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 40 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.cc
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#define MOD 1000000000LL
+#define ll long long
+#define INF 1e9 + 10
+#define lINF 1e18 + 10
+using namespace std;
+
+int arr[501][501];
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                    arr[i][j] = arr[i][k] + arr[k][j];
+                }
+            }
+        }
+    }
+
+    while (m--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        if(arr[a][b] <= c) cout<<"Enjoy other party\n";
+        else cout <<"Stay here\n";
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}

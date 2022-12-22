70ac17517607cd38f48536bfe65b98631334c43d
GoGradually <62929862+GoGradually@users.noreply.github.com>
2022-12-22 17:17:15 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#define MOD 1000000000LL
+#define ll long long
+#define INF 1e9 + 10
+#define lINF 1e18 + 10
+using namespace std;
+
+void solve() {
+    int t;
+    cin>>t;
+    for (int i = 1; i<=t; i++) {
+        for (int j = 0; j<i; j++) {
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}

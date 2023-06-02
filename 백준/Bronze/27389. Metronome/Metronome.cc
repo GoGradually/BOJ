a84f34a2885f5557c22092336853a603a8899228
GoGradually <62929862+GoGradually@users.noreply.github.com>
2023-06-02 12:32:12 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 1e9 + 10
+#define lINF 1e18 + 10
+#define MOD 1000000007LL;
+/*
+#include "atcoder/modint.hpp"
+using mint = atcoder::modint998244353;
+*/
+
+using namespace std;
+
+void Solve() {
+    double n;
+    cin >> n;
+    cout << n / 4.0 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout<<fixed;
+    cout.precision(2);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}

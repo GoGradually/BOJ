9b71b2e00264df3f925a9f09d6271163fd78439f
GoGradually <62929862+GoGradually@users.noreply.github.com>
2023-08-09 00:45:57 +0900

    [Gold IV] Title: 점수따먹기, Time: 28 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<ll>> arr(n, vector<ll>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    vector<vector<ll>> psum(n + 1, vector<ll>(m + 1));
+    for (int i = 0; i < n; i++) {
+        vector<ll> psum2(m + 1);
+        for (int j = 0; j < m; j++) {
+            psum2[j + 1] = psum2[j] + arr[i][j];
+            psum[i + 1][j + 1] = psum[i][j + 1] + psum2[j + 1];
+        }
+    }
+    ll ans = -10000;
+    for (int i = 1; i <= m; i++) {
+        for (int j = 0; j < i; j++) {
+            ll prev = 0;
+            for (int k = 1; k <= n; k++) {
+                ll val =
+                    psum[k][i] - psum[k][j] - psum[prev][i] + psum[prev][j];
+                ans = max(ans, val);
+                if (val < 0) prev = k;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하고 체계적인 상태를 유지
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 문제의 "맥" 잡고있기, 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 예제 직접 전개(수식/머릿속 시각화) &
+// 그리디한 "추론"(증명)-> -자료구조/알고리즘의 필요성 느끼기

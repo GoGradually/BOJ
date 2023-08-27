240588c931833e1188edcec9b391384b003e03fb
GoGradually <62929862+GoGradually@users.noreply.github.com>
2023-08-27 15:30:47 +0900

    [Gold III] Title: Teamwork, Time: 12 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Gold/16765. Teamwork/Teamwork.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF (int)1e9 + 10
+#define lINF (ll)1e18 + 10LL
+const ll MOD = 1000000007LL;
+// #include <atcoder/modint.hpp>
+// using mint = atcoder::modint998244353;
+
+using namespace std;
+
+void Solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<ll> dp(10001, 0);
+    for (int i = 1; i <= n; i++) {
+        ll mx = arr[i - 1];
+        for (ll j = 1; j <= k && j <= i; j++) {
+            mx = max(mx, arr[i - j]);
+            dp[i] = max(dp[i], dp[i - j] + mx * j);
+        }
+    }
+    cout << dp[n] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    while (T--) Solve();
+    return 0;
+}
+
+/*
+찾아야 할 것들
+*int 오버플로우, out of bounds
+*특수한 경우(n=1?)
+*아무것도 하지 않는 대신 무언가를 수행하기. 체계적인 상태를 유지.
+*적어두기
+*한가지 접근 방식에 얽메이지 말기
+*/
+// 알고리즘의 작동방식 "완전히" 이해하려 노력하기
+// 수행 목표
+// 1. "추론"(greedP, 증명으로 아이디어)
+// 2. 알고리즘 "처음"부터 풀이과정 직접 전개, cutting
+// 3. "구현"
